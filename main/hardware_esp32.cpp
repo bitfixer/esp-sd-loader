13357ef2418a43a8e748be304e5651789119b295
Michael Hill <bitfixer@bitfixer.com>
2025-09-30 01:12:47 -0700

    remove old sd card code

--- main/hardware_esp32.cpp
+++ main/hardware_esp32.cpp
@@ -2,28 +2,12 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <driver/gpio.h>
-#include <driver/spi_common.h>
-#include <driver/spi_master.h>
 #include <string.h>
 #include <esp_log.h>
 
 #define LED_PIN     2
 
-// esp32
-// TODO: add pins
-
-// esp32s2
-#define CS_PIN      4
-#define MISO_PIN    37
-#define MOSI_PIN    35
-#define SCK_PIN     36
-
 static constexpr const char* TAG = "HW";
-
-uint8_t spi_cs()
-{
-    return CS_PIN;
-}
 
 void init_led()
 {
@@ -46,69 +30,6 @@ void set_led(bool value)
 void hDelayMs(int ms)
 {
     vTaskDelay(pdMS_TO_TICKS(ms));
-}
-
-// SPI
-
-static spi_device_handle_t _spi;
-static bool _spi_init = false;
-
-void spi_init() {
-    ESP_LOGW(TAG, "spi_init");
-    if (_spi_init) {
-        return;
-    }
-    gpio_set_direction((gpio_num_t)CS_PIN, GPIO_MODE_OUTPUT);
-    spi_cs_unselect();
-
-    esp_err_t ret;
-    spi_bus_config_t buscfg = {
-        .mosi_io_num = MOSI_PIN,
-        .miso_io_num = MISO_PIN,
-        .sclk_io_num = SCK_PIN,
-        .quadwp_io_num = -1,
-        .quadhd_io_num = -1,
-        .max_transfer_sz = 0,  // default
-    };
-
-    ret = spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_DISABLED);
-    _spi_init = true;
-    ESP_ERROR_CHECK(ret);
-
-    spi_device_interface_config_t devcfg = {};
-    devcfg.clock_speed_hz = 25 * 1000 * 1000;  // 25 MHz
-    devcfg.mode = 0;
-    devcfg.spics_io_num = -1;  // <--- NO CS pin
-    devcfg.queue_size = 1;
-
-    ret = spi_bus_add_device(SPI2_HOST, &devcfg, &_spi);
-    ESP_ERROR_CHECK(ret);
-    ESP_LOGW(TAG, "SPI initialized without CS");
-}
-
-uint8_t spi_transmit(uint8_t data) {
-    uint8_t rx_byte = 0;
-
-    spi_transaction_t trans = {
-        .length = 8,  // bits
-        .tx_buffer = &data,
-        .rx_buffer = &rx_byte,
-    };
-
-    esp_err_t ret = spi_device_transmit(_spi, &trans);
-    ESP_ERROR_CHECK(ret);
-
-    return rx_byte;
-}
-
-void spi_cs_select()
-{
-    gpio_set_level((gpio_num_t)CS_PIN, 0);
-}
-
-void spi_cs_unselect()
-{
-    gpio_set_level((gpio_num_t)CS_PIN, 1);
 }
 
 bool isFirmwareFile(char* fname)
--- main/helpers.h
+++
@@ -1,9 +0,0 @@
-#ifndef __helpers_h__
-#define __helpers_h__
-
-#include <stddef.h>
-#define PACKED __attribute__ ((packed))
-
-void upperStringInPlace(char* str);
-
-#endif
